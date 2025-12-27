d84fa17d7037f8a87a2fdeb5c7f6398cb4a4edb3
Miroslav Adrian Axmann <axmann.pracovni@gmail.com>
2025-12-27 12:10:11 +0100

    Initial Commit

---
+++ FitnessTracker Watch App/FitnessTrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  FitnessTrackerApp.swift
+//  FitnessTracker Watch App
+//
+//  Created by Miroslav Adrian Axmann on 27.12.2025.
+//
+
+import SwiftUI
+
+@main
+struct FitnessTracker_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ FitnessTracker/FitnessTrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  FitnessTrackerApp.swift
+//  FitnessTracker
+//
+//  Created by Miroslav Adrian Axmann on 27.12.2025.
+//
+
+import SwiftUI
+
+@main
+struct FitnessTrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
